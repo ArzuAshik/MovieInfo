ee860f254f32f0606c6304f7e71ce5fec7105af4
ArzuAshik <arzu2050@gmail.com>
2020-08-18 14:27:02 +0600

    single page done

---
+++ main.js
@@ -0,0 +1,35 @@
+const searchForm = document.getElementById('searchForm');
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const searchOutput = document.getElementById('searchOutput');
+
+
+function searchAction(){
+    fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=9360d14e`)
+        .then(resp => resp.json())
+        .then(data => {
+            movies = data.Search;
+            let output = '';
+            movies.forEach(movie => {               
+                output += 
+                `
+                <div class="col-md-3 text-center bg-info py-2">
+                    <img class="img-fluid"  height="350" width="auto" src="${movie.Poster}" alt="">
+                    <h2>${movie.Title}</h2>
+                    <h6>Year: ${movie.Year}</h6>
+                    <button onclick='movieDetails("${movie.imdbID}")' class="btn btn-primary">Details</button>
+                </div>
+                `;
+            });
+            searchOutput.innerHTML = output;
+        })
+}
+
+function movieDetails(id){
+    console.log(id);
+}
+
+searchForm.addEventListener('submit', (event) => {
+    searchAction();
+    event.preventDefault();
+})
